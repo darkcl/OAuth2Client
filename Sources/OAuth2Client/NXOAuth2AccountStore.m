bfa047d735bcb4a94e3e055b05b2276661f76407
Yeung Yiu Hung <hkclex@gmail.com>
2017-09-12 20:33:29 +0800

    Allow access to keychain all the time on iOS

--- Sources/OAuth2Client/NXOAuth2AccountStore.m
+++ Sources/OAuth2Client/NXOAuth2AccountStore.m
@@ -689,6 +689,7 @@ NSString * const kNXOAuth2AccountStoreAccountType = @"kNXOAuth2AccountStoreAccou
     NSDictionary *result = nil;
     NSMutableDictionary *query = [NSMutableDictionary dictionaryWithObjectsAndKeys:
                                   (__bridge NSString *)kSecClassGenericPassword, kSecClass,
+                                  kSecAttrAccessibleAlways, kSecAttrAccessible,
                                   serviceName, kSecAttrService,
                                   kCFBooleanTrue, kSecReturnAttributes,
                                   nil];
